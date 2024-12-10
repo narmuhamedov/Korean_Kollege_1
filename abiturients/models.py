a1f4ddab781129acc87f49527dfff697161da78b
narmuhamedov <kingradomir@gmail.com>
2024-12-10 12:19:49 +0600

    stud_live_front

--- abiturients/models.py
+++ abiturients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class AllQuestion(models.Model):
+    question_text = models.CharField(max_length=200, verbose_name='Добавьте вопрос')
+    answer_text = models.URLField(verbose_name='Вставьте ссылку на документ к вопросу из DRIVE')
+
+    def __str__(self):
+        return self.question_text
+
+
+class EditionInformation(models.Model):
+    question_text = models.CharField(max_length=200, verbose_name='Добавьте вопрос')
+    answer_text = models.URLField(verbose_name='Вставьте ссылку на документ к вопросу из DRIVE')
+
+    def __str__(self):
+        return self.question_text
--- abiturients/views.py
+++ abiturients/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from main_page.models import ITCourses, Courses
+from . import models
+
 
 
 def abituents_list(request):
     if request.method == "GET":
         courses = Courses.objects.all()
         itcourses = ITCourses.objects.all()
+        all_questions = models.AllQuestion.objects.all()
+        edition_questions = models.EditionInformation.objects.all()
         context = {
             "courses": courses,
             "itcourses": itcourses,
+            "all_questions": all_questions,
+            "edition_questions": edition_questions,
         }
     return render(request, template_name='abit/abit_list.html', context=context)
